32a482e2e154deeb5a9a8fda7b4f489785abe500
celinaLind <clind323@gmail.com>
2024-08-04 19:14:55 -0700

    add styling and page tabs for home, pantry, and recipe

--- app/page.js
+++ app/page.js
@@ -1,40 +1,85 @@
 'use client';
 import * as React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import { lightBlue, lime } from '@mui/material/colors';
-import { app, firestore } from '../firebase';
+import { Box, Button, Typography, TextField, Container, Tabs, Tab, CustomTabPanel, Card, CardHeader, CardContent, CardActions } from '@mui/material';
+import { firestore, auth } from '../firebase';
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore/lite';
+import { collection, getDoc, doc, query, setDoc, deleteDoc, getDocs } from 'firebase/firestore';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import Pantry from './pantry/page';
+import RecipePage from './recipes/page';
+
 
 export default function Home() {
-  const [pantryItems, setPantryItems] = useState([]);
-  useEffect(() => {
-    const updateItems = async () => {
-      const querySnapshot = await getDocs(collection(firestore, "pantry"));
-      const items = [];
-      querySnapshot.forEach((doc) => {
-        items.push(doc.id);
-      });
-      console.log(items);
-      setPantryItems(items);
+const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      </div>
+    );
+  }
+  
+  CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
     };
-    updateItems();
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(firestore, 'users', 'user1');
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setUser(docSnap.data());
+      }
+    }
+    fetchData();
   }, []);
-  return (
-    <Box width='100vw' height='100vh' display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'}>
-      <Box border={1} overflow="hidden">
-        <Box height={'100px'} width={'800px'} alignContent={'center'} sx={{ textAlign: 'center', backgroundColor: lime[100] }}>
-          <Typography variant='h2'>Pantry Tracker</Typography>
-        </Box>
-        <Box width="100%" height="300px" sx={{ overflow: 'auto' }}>
-        <Stack spacing={2}>
-          {pantryItems.map((item, index) => (
-            <Box key={index} sx={{ textAlign: 'center', backgroundColor: lightBlue[50]}}>
-              <Typography variant="h4">{ item }</Typography>
-            </Box>
-          ))}
-        </Stack>
-        </Box>
-      </Box></Box>
-  );
+ return (
+  <Container id="main-body" maxWidth="100vw" height="100vh">
+    
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+  <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+    <Tab label="Home" {...a11yProps(0)} />
+    <Tab label="Pantry" {...a11yProps(1)} />
+    <Tab label="Recipes" {...a11yProps(2)} />
+  </Tabs>
+</Box>
+<CustomTabPanel value={value} index={0}>
+  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2, margin: 'auto' }}>
+      <Typography variant="h3">Welcome to Your Pantry</Typography>
+    </Box>
+</CustomTabPanel>
+<CustomTabPanel value={value} index={1}>
+  <Pantry></Pantry>
+</CustomTabPanel>
+<CustomTabPanel value={value} index={2}>
+<RecipePage></RecipePage>
+</CustomTabPanel>
+    
+  </Container>
+  
+ );
+
 }
