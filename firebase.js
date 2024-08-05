02b1cea7e5ab807fe573d9998415ca52964f44ad
celinaLind <clind323@gmail.com>
2024-08-04 21:12:04 -0700

    fix linking and connection call

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCefLRh0ysIDLJM22s7ufqFlSgz0cYWt4M",
-  authDomain: "hspantry-ab58c.firebaseapp.com",
-  projectId: "hspantry-ab58c",
-  storageBucket: "hspantry-ab58c.appspot.com",
-  messagingSenderId: "831696011389",
-  appId: "1:831696011389:web:ef6ced0d7e30212fe6f114",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
 //   measurementId: "G-RCPGHYB7X5"
 };
 
